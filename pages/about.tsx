c6d5ef90aff7be7df684fd2cb95b8d7500f7fd6d
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-03-15 21:15:27 +0700

    create about page

---
+++ pages/about.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from "next"
+import styles from '../styles/Home.module.css'
+
+const About: NextPage = () => {
+  return (
+    <div>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          About
+        </h1>
+      </main>
+    </div>
+  )
+}
+
+export default About
