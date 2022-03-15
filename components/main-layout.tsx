043520f00c347067a1cb52c5344d79708c5d2e6d
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-03-15 21:16:25 +0700

    create menu and apply to app layout

---
+++ components/main-layout.tsx
@@ -0,0 +1,19 @@
+import { NextComponentType } from "next";
+import Link from "next/link";
+
+
+const MainLayout: NextComponentType = ({children}) => {
+    return (
+        <div>
+            <Link href="/">
+                <a>Home</a>
+            </Link>
+            <Link href="/about">
+                <a>About</a>
+            </Link>
+            {children}
+        </div>
+    )
+}
+
+export default MainLayout
