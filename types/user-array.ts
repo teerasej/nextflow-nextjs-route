6ea1f82bebb9a1c8311983af762c06fad9034d37
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-03-16 08:05:02 +0700

    define user's array and user's type

--- types/user-array.ts
+++ types/user-array.ts
@@ -1,9 +1,11 @@
 
 
-export type UserArray = [{
+export type UserArray = [User]
+
+export type User = {
     id:Number,
     firstName:string,
     lastName:string,
     email:string,
     gender:string
-}]
+}
