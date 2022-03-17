43102a9d632d7125cbe79e391172070e559bc911
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-03-17 07:35:48 +0700

    clear for start

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,34 +3,11 @@ import type { AppProps } from 'next/app'
 import MainLayout from '../components/main-layout'
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
-import NProgress from 'nprogress'
-import './../styles/nprogress.css'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
 
-  const router = useRouter()
-
-  useEffect(() => {
-    const handleStart = (url:string) => {
-      console.log(`Loading: ${url}`)
-      setTimeout(()=>{}, 3000)
-      NProgress.start()
-    }
-    const handleStop = () => {
-      NProgress.done()
-    }
-
-    router.events.on('routeChangeStart', handleStart)
-    router.events.on('routeChangeComplete', handleStop)
-    router.events.on('routeChangeError', handleStop)
-
-    return () => {
-      router.events.off('routeChangeStart', handleStart)
-      router.events.off('routeChangeComplete', handleStop)
-      router.events.off('routeChangeError', handleStop)
-    }
-  }, [router])
-
+  
 
   return (
     <MainLayout>
